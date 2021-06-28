5a84bb630ce329a17d3cddbafae2cd7f0117a730
mdberkey <meberkey@wisc.edu>
2021-06-28 17:30:53 -0500

    Add task_helper.py

--- src/backend.py
+++ src/backend.py
@@ -1,12 +1,12 @@
 import pandas as pd
-import src.autoshaping as autoshaping
+import src.autoshaping.autoshaping as autoshaping
 
 
 # TODO: import other tasks
 
 
 class Backend:
-    """ Class for backend """
+    """ backend functions"""
 
     # TODO: add other tasks to self.tasks
     def __init__(self):
@@ -49,70 +49,9 @@ class Backend:
         """
         Starts specified task
         :param name: Name of task dir and .py file
-        :return: None
+        :return: True if task completed
         """
         self.tasks[name].main()
-
-
-# TODO: finish class
-class TaskHelper:
-    """ Helper functions for tasks"""
-
-    @staticmethod
-    def read_params():
-        """
-        Reads parameters from parameters.csv
-        :return: parameters in dictionary and dataframe format
-        """
-        params_df = pd.read_csv('parameters.csv')
-        params_dict = params_df.to_dict(orient='list')
-        for key in params_dict.keys():
-            params_dict[key] = params_dict[key][0]
-
-        return params_dict, params_df
-
-    @staticmethod
-    def dispense_pellet(num=1):
-        """
-        Dispenses set number of food pellets
-        :param num: number of pellets
-        :return: None
-        """
-        #TODO: ADD hardware support for pellet dispenser
-        print('pellet')
-
-    @staticmethod
-    def start_light(on=True):
-        """
-        Turns on/off start_light
-        :param on: turns light on if True, off if False
-        :return: None
-        """
-        #TODO: ADD hardware support for start light
-        print('start light: ' + on)
-
-    @staticmethod
-    def stim_lights(on=(True, True)):
-        """
-        Turns on/off stim_lights
-        :param on: (left light, right light) = (bool, bool)
-        :return: None
-        """
-        #TODO: ADD hardware support for stim lights
-        print('stim lights:' + on)
-
-    @staticmethod
-    def levers(input_ratio=1):
-        """
-        Deploys and gets input from levers
-        :param input_ratio: Input ratio for levers
-        :return: lever results
-        """
-        #TODO: ADD hardware support for levers
-        # TODO levers come out
-        for i in range(input_ratio):
-            # TODO await lever press
-            print('lever pressed')
         return True
 
 
--- src/frontend.py
+++ src/frontend.py
@@ -1,10 +1,13 @@
 import PySimpleGUI as sg
 import src.backend as be
+import pandas as pd
+
 
 class Frontend:
+    """ frontend GUI """
 
     def __init__(self, backend=be.Backend(), size=(500, 300), font='Helvetica',
-                 header_font="helvetica 15 underline bold", pad=[5, 5]):
+                 header_font='helvetica 15 underline bold', pad=[5, 5]):
         self.backend = backend
         self.size = size
         self.font = font
@@ -13,47 +16,81 @@ class Frontend:
 
     def main_menu(self):
         tasks = self.backend.tasks
-
         task_col = [[sg.Button(task, pad=self.pad)] for task in tasks.keys()]
-        task_col.insert(0, [sg.Text("Tasks", font=self.header_font)])
+        task_col.insert(0, [sg.Text('Tasks', font=self.header_font)])
         option_col = [
-            [sg.Text("Other", font=self.header_font)],
-            [sg.Button("Global Parameters", pad=self.pad)],
-            [sg.Button("Export Data", pad=self.pad)],
-            #[sg.Button("Quick View Data", pad=self.pad)],
-            [sg.Button("Delete Data", pad=self.pad)],
-            [sg.Button("QUIT", pad=self.pad)]
+            [sg.Text('Other', font=self.header_font)],
+            [sg.Button('Export Data', pad=self.pad)],
+            [sg.Button('Delete Data', pad=self.pad)],
+            [sg.Button('QUIT', pad=self.pad)]
         ]
 
         layout = [
             [sg.Column(task_col), sg.VSeparator(), sg.Column(option_col)],
-            [sg.Text("For Info/Help: Please refer to the user manual.")],
-            [sg.Text("Source Code: https://github.com/mdberkey/lever-tasks")]
+            [sg.Text('For Info/Help: Please refer to the user manual.')],
+            [sg.Text('Source Code: https://github.com/mdberkey/lever-tasks')]
         ]
 
-        main_window = sg.Window("Lever Tasks v0.1", layout, margins=self.size, font=self.font)
+        main_window = sg.Window('Lever Tasks v0.1', layout, margins=self.size, font=self.font)
 
         while True:
             event, values = main_window.read()
-
-            if event == "QUIT" or event == sg.WIN_CLOSED:
+            if event == 'QUIT' or event == sg.WIN_CLOSED:
                 break
-            elif event == "Global Parameters":
-                self.params_menu()
-            elif event == "Export Data":
+            elif event == 'Export Data':
                 self.export_data(tasks)
-            elif event == "Delete Data":
+            elif event == 'Delete Data':
                 self.delete_data(tasks)
-            elif event == "Quick View Data":
-                self.quick_view_data(tasks)
             else:
-                for task in tasks:
-                    if event == task.name:
-                        self.params_menu(task)
+                self.params_menu(event)
         main_window.close()
 
-    def params_menu(self):
+    def params_menu(self, task, is_task=True):
+        params = self.backend.read_params()[0]
+        params_col = []
+        for key, value in params.items():
+            params_col.append([sg.Text(key, size=(25, 1)), sg.InputText(value, key=key)])
+
+        layout = [
+            [sg.Column(params_col)],
+            [sg.Button('Back to Main Menu'), sg.Button('Confirm Parameters')],
+        ]
+
+        layout.append([sg.Text('Note: You must \'Confirm Parameters\' before starting task.')])
+        start_button = sg.Button('Start Task', disabled=True, key='ST')
+        layout[1].append(start_button)
+
+        params_window = sg.Window(task + ' Parameters', layout, margins=self.size, font=self.font)
+        while True:
+            event, values = params_window.read()
+
+            if event == 'Back to Main Menu' or event == sg.WIN_CLOSED:
+                break
+            elif event == 'Confirm Parameters':
+                for key, value in values.items():
+                    if key == 'date':
+                        try:
+                            assert (list(map(int, value.split('/'))))
+                        except ValueError:
+                            sg.Popup('Parameter Error: Please put date format as: \'mm/dd/yy\'', font=self.font)
+                    values[key] = [value]
+
+                self.backend.set_params(pd.DataFrame.from_dict(values, orient='columns'))
+                try:
+                    start_button.update(disabled=False)
+                except UnboundLocalError:
+                    pass
+            elif event == 'ST':
+                if self.backend.start_task(name=task):
+                    sg.Popup('Task Completed.', font=self.font)
+        params_window.close()
+
+    def export_data(self):
         pass
+
+    def delete_data(self):
+        pass
+
 
 if __name__ == '__main__':
     frontend = Frontend()
