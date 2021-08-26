a9641e2c58d898678285098598dd180a44c2cecb
mdberkey <meberkey@wisc.edu>
2021-08-26 17:01:19 -0500

    Update relay_cdll.py

--- src/usb_relay/relay_cdll.py
+++ src/usb_relay/relay_cdll.py
@@ -4,7 +4,7 @@ from ctypes import CDLL, RTLD_GLOBAL
 def main():
     path = '/lib/arm-linux-gnueabihf/libusb-1.0.so.0.1.0'
     lib = CDLL(path, mode=RTLD_GLOBAL)
-    relay = CDLL('/usb_relay/relay.so')
+    relay = CDLL('usb_relay/relay.so')
     relay.main()
 
 
